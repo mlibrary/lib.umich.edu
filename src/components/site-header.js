029253a19d7015b2f8e638c194a203698e4d0466
Jon Earley <jonathan.a.earley@gmail.com>
2019-02-12 13:14:58 -0500

    Update site navigation component to use api/sitemenu data.

--- src/components/site-header.js
+++ src/components/site-header.js
@@ -10,49 +10,17 @@ const StyledLink = styled(Link)({
   }
 })
 
-const getTopLevelNav = (data) => {
-  const topLevelNavData = data.allNodeLandingPage.edges[0].node.relationships.node__page
-  const navData = topLevelNavData.reduce((acc, page) => {
-    if (page.relationships.node__page) {
-      return acc.concat({
-        text: page.title,
-        to: page.path.alias
-      })
-    }
-
-    return acc
-  }, [])
-
-  return navData
-}
-
 export default () => (
   <StaticQuery
     query={graphql`
-      fragment PageFields on node__page {
-        title
-        path {
-          alias
-        }
-      }
       {
-        allNodeLandingPage {
-          edges {
-            node {
-              title
-              path {
-                alias
-              }
-              relationships {
-                node__page {
-                  ...PageFields
-                  relationships {
-                    node__page {
-                      ...PageFields
-                    }
-                  }
-                }
-              }
+        navigation(id: { eq: "navigation" } ) {
+          data {
+            text
+            to
+            children {
+              text
+              to
             }
           }
         }
@@ -60,7 +28,7 @@ export default () => (
     `}
     render={data => (
       <Header
-        nav={getTopLevelNav(data)}
+        nav={data.navigation.data.children}
         renderAnchor={data => (
           <StyledLink
             to={data.to}
