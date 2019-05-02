006ed62353799f7e48112db5697e83daa70fc103
Jon Earley <jonathan.a.earley@gmail.com>
2019-05-01 21:52:16 -0400

    umich-lib/core alpha.5

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,22 +4,11 @@ import Layout from "../components/layout"
 import HTML from "../components/html"
 import styled from 'react-emotion'
 import {
-  MEDIA_QUERIES,
   Margins,
   Heading,
   SPACING,
   Alert,
 } from '@umich-lib/core'
-
-const StyledGrid = styled('div')({
-  [MEDIA_QUERIES.LARGESCREEN]: {
-    display: 'grid',
-    gridTemplateColumns: 'auto 14rem',
-    gridTemplateRows: 'max-content',
-    gridTemplateAreas: `"main side"`,
-    gridColumnGap: '2rem'
-  }
-})
 
 const Prose = styled('div')({
   '> *:not(:last-child)': {
@@ -30,35 +19,31 @@ const Prose = styled('div')({
 const PageTemplate = ({ data }) => {
   const {
     title,
-    relationships,
-    fields,
     body
   } = data.nodePage
 
   return (
     <Layout>
       <Margins>
-        <StyledGrid>
-          <main
-            style={{
-              gridArea: 'main',
-              marginTop: SPACING['XL']
-            }}
-          >
-            <Prose>
-              <Heading size={"3XL"}>{title}</Heading>
-              {body && body.format === 'basic_html' ? (
-                <HTML html={body.value} />
-              ) : (
-                <Alert intent="warning">
-                  <span style={{ fontSize: '1rem' }}>
-                    This page does not have body content.
-                  </span>
-                </Alert>
-              )}
-            </Prose>
-          </main>
-        </StyledGrid>
+        <main
+          style={{
+            maxWidth: '38rem',
+            marginTop: SPACING['XL']
+          }}
+        >
+          <Prose>
+            <Heading size={"3XL"}>{title}</Heading>
+            {body && body.format === 'basic_html' ? (
+              <HTML html={body.value} />
+            ) : (
+              <Alert intent="warning">
+                <span style={{ fontSize: '1rem' }}>
+                  This page does not have body content.
+                </span>
+              </Alert>
+            )}
+          </Prose>
+        </main>
       </Margins>
     </Layout>
   )
