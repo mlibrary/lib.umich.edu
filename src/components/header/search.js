32bc710a9acb4f1af330e682d7891bd07139cb2a
Jon Earley <jonathan.a.earley@gmail.com>
2019-06-10 10:33:42 -0400

    .

--- src/components/header/search.js
+++ src/components/header/search.js
@@ -24,7 +24,7 @@ function Search() {
     >
       <TextInput
         id="search-query"
-        labelText="Search terms"
+        labelText="Query"
         type="search"
         hideLabel
         name="query"
