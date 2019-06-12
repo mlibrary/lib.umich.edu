e5459b7ef4bc7c706df8c1c86819f969e07d9292
Jon Earley <jonathan.a.earley@gmail.com>
2019-06-11 22:43:43 -0400

    Reverting back manually.

--- src/templates/location.js
+++ src/templates/location.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import { graphql } from "gatsby"
 
-import {
-  Heading,
-  Text,
-  Margins,
-  SPACING
-} from '@umich-lib/core'
-
 import Layout from "../components/layout"
 import PageHeader from '../components/page-header'
-import Link from '../components/link'
 
 export default function({ data }) {
   const {
     title,
     body,
     fields,
-    relationships,
-    field_building_official_name,
-    field_building_address
+    relationships
   } = data.page
-
-  const {
-    address_line1,
-    locality,
-    postal_code,
-    administrative_area
-  } = field_building_address
-
-  //const directions_base_url = "https://www.google.com/maps/dir/?api=1"
-  //const directions_destination_query = encodeURIComponent(`${field_building_official_name} ${address_line1} ${locality}, ${administrative_area} ${postal_code}`)
 
   return (
     <Layout>
@@ -40,18 +20,10 @@ export default function({ data }) {
         breadcrumb={fields.breadcrumb}
         imageData={relationships.field_image[0].localFile.childImageSharp.fluid}
         ariaLabel="Location description"
-        css={{
-          marginBottom: SPACING['2XL']
-        }}
       />
-
-      <Margins>
-        <article aria-label="Hours, parking, access, and amenities">
-          
-        </article>
-      </Margins>
     </Layout>
   )
+
 }
 
 export const query = graphql`
@@ -68,13 +40,6 @@ export const query = graphql`
           to
           text
         }
-      }
-      field_building_official_name
-      field_building_address {
-        address_line1
-        locality
-        postal_code
-        administrative_area
       }
       relationships {
         field_image {
