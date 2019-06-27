1f5401ba23793eaafc470e39b2b1cc57df4e5780
Jon Earley <jonathan.a.earley@gmail.com>
2019-06-27 15:52:10 -0400

    More query setup.

--- src/templates/section.js
+++ src/templates/section.js
@@ -2,33 +2,33 @@ import React from 'react'
 import { graphql } from "gatsby"
 
 import {
-  Margins,
-  Heading,
-  SPACING
+  Margins
 } from '@umich-lib/core'
 
 import Layout from "../components/layout"
 import SEO from '../components/seo'
-import {
-  Template,
-  Top,
-  Side,
-  Content
-} from '../components/page-layout'
-import HTML from '../components/html'
-import Breadcrumb from '../components/breadcrumb'
-import SideNavigation from '../components/side-navigation'
-import Panels from '../components/panels'
+import PageHeader from '../components/page-header'
 
 function SectionTemplate({ data, ...rest }) {
   const {
-    title
+    title,
+    field_header_title,
+    field_horizontal_nav_title,
+    fields,
+    body
   } = data.page
 
   return (
     <Layout>
       <SEO title={title} />
-      {title}
+      <PageHeader
+        breadcrumb={fields.breadcrumb}
+        title={field_header_title}
+        summary={body ? body.summary : null}
+      />
+      <Margins>
+        {field_horizontal_nav_title}
+      </Margins>
     </Layout>
   )
 }
@@ -39,6 +39,11 @@ export const query = graphql`
   query($slug: String!) {
     page: nodeSectionPage(fields: { slug: { eq: $slug } }) {
       title
+      field_header_title
+      field_horizontal_nav_title
+      fields {
+        breadcrumb
+      }
     }
   }
 `
