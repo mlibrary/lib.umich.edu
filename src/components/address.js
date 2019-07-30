85fdc7ed00d5826c44b62267215145e010495e8c
Jon Earley <jonathan.a.earley@gmail.com>
2019-07-30 12:21:18 -0400

    Handle address Card conditions.

--- src/components/address.js
+++ src/components/address.js
@@ -6,7 +6,7 @@ function Address({ data, ...rest }) {
     address_line1,
     postal_code,
     administrative_area
-  } = data
+  } = processAddressData(data)
 
   return (
     <address {...rest}>
@@ -17,3 +17,25 @@ function Address({ data, ...rest }) {
 }
 
 export default Address
+
+function processAddressData(data) {
+  /*
+    Each Drupal content type has it's own
+    conditions for displaying address.
+  */
+  switch (data.__typename) {
+    case 'node__room':
+      return data.relationships.field_room_building.field_building_address
+    case 'node__building':
+      return data.field_building_address
+    case 'node__location':
+      if (data.field_address_is_different_from_ === true) {
+        return data.field_building_address
+      }
+      if (data.relationships.field_parent_location) {
+        return data.relationships.field_parent_location.field_building_address
+      }
+    default:
+      return undefined
+  }
+}
