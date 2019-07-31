6be653686c795b863b53b220b12e9026e362285c
Jon Earley <Jonathan.a.earley@gmail.com>
2019-07-31 14:09:10 -0400

    Trying to add fetch-retry. (#13)
    Add fetch-retry for Drupal rest view fields.

---
+++ plugins/gatsby-source-umich-lib/fetch.js
@@ -0,0 +1,19 @@
+const fetch = require('fetch-retry');
+
+function fetchWithRetry(url) {
+  return fetch(url, {
+    retries: 5,
+    retryOn: [500],
+    retryDelay: function(attempt) {
+      return Math.pow(2, attempt) * 1000; // 1000, 2000, 4000
+    }
+  }).then(function(response) {
+    if (response.status !== 200) {
+      console.log('Response status:', response.status)
+    }
+
+    return response.json();
+  })
+}
+
+exports.fetch = fetchWithRetry
