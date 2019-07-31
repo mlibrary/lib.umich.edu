f312906c9ce9f8050683a610c4bf15dadee3e934
Jon Earley <jonathan.a.earley@gmail.com>
2019-07-31 14:04:09 -0400

    Retry on 500 error

--- plugins/gatsby-source-umich-lib/fetch.js
+++ plugins/gatsby-source-umich-lib/fetch.js
@@ -3,6 +3,7 @@ const fetch = require('fetch-retry');
 function fetchWithRetry(url) {
   return fetch(url, {
     retries: 5,
+    retryOn: [500],
     retryDelay: function(attempt) {
       return Math.pow(2, attempt) * 1000; // 1000, 2000, 4000
     }
