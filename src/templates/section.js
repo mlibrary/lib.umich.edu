2803ee43d45b440440fb47141dc7541a7061bd10
Jon Earley <jonathan.a.earley@gmail.com>
2019-08-06 13:03:06 -0400

    Lookup all hour paragraphs.

--- src/templates/section.js
+++ src/templates/section.js
@@ -223,7 +223,95 @@ export const query = graphql`
         endhours
       }
     }
-    ... on paragraph__summer_term_hours {
+    ... on paragraph__fall_study_break {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__thanksgiving_break_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__fall_exam_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__winter_break_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__intersession_after_new_year {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__spring_break_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__winter_exam_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__intersession_after_winter_exams {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__memorial_day_holiday_hours {
       field_date_range {
         value
         end_value
@@ -235,6 +323,51 @@ export const query = graphql`
       } 
     }
     ... on paragraph__spring_term_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+        comment
+      } 
+    }
+    ... on paragraph__july_4_holiday_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__summer_term_hours {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__intersession_after_summer_exams {
+      field_date_range {
+        value
+        end_value
+      }
+      field_hours_open {
+        day
+        starthours
+        endhours
+      } 
+    }
+    ... on paragraph__hours_exceptions {
       field_date_range {
         value
         end_value
