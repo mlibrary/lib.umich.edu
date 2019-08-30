cfe804598e1e9d46db245eb2960d4696a7d0216b
Jon Earley <jonathan.a.earley@gmail.com>
2019-08-29 21:02:27 -0400

    Remove polyfill console.log.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,7 +12,6 @@ export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === `undefined`) {
     import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
   }
 }
 
