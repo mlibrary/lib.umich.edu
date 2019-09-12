f320e6c3d472d246627f5f93db3b231693182629
Jon Earley <jonathan.a.earley@gmail.com>
2019-09-12 12:20:50 -0400

    Experimenting with updating footer -- LIBWEB-442.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,29 +3,102 @@ import React from 'react'
 import {
   COLORS,
   SPACING,
-  Margins
+  Margins,
+  Heading,
+  MEDIA_QUERIES,
 } from '@umich-lib/core'
 
-function Footer() {
-  return (
-    <footer css={{
-      minHeight: '14rem',
-      background: COLORS['teal']['500']
-    }}>
+import Link from './link'
 
+function FooterHeading(props) {
+  return (
+    <Heading
+      level={2}
+      size="3XS"
+      css={{
+        color: COLORS.blue['200'],
+      }}
+      {...props}
+    />
+  )
+}
+
+function Footer() {
+  const now = new Date()
+  const year = now.getFullYear()
+
+  return (
+    <footer
+      css={{
+        background: COLORS['blue']['400'],
+      }}
+    >
       <Margins>
-        <div css={{
-          color: 'white',
-          padding: `${SPACING['2XL']} 0`
-        }}>
-          [Logo component here]
-          <address css={{
-            marginTop: SPACING['L']
-          }}>
-            <p>913 S. University Avenue<br/>Ann Arbor, MI 48109</p>
-          </address>
+        <div
+          css={{
+            [MEDIA_QUERIES.LARGESCREEN]: {
+              columns: '2',
+              columnGap: SPACING['3XL'],
+              '& section': {
+                breakInside: 'avoid',
+                marginBottom: '0',
+              },
+            },
+            color: 'white',
+            padding: `${SPACING['3XL']} 0`,
+            '& section:not(:last-of-type)': {
+              marginBottom: SPACING['XL'],
+            },
+          }}
+        >
+          <section>
+            <FooterHeading>University of Michigan Library</FooterHeading>
+            <address
+              css={{
+                marginTop: SPACING['2XS'],
+              }}
+            >
+              <p>
+                913 S. University Avenue
+                <br />
+                Ann Arbor, MI 48109
+              </p>
+            </address>
+            <p>
+              <Link kind="light" to="tel:+1-734-764-0400">
+                (734) 764-0400
+              </Link>
+            </p>
+            <p>
+              <Link kind="light" to="/">
+                Send us an email
+              </Link>
+            </p>
+          </section>
+          <section>
+            <FooterHeading>Privacy and copyright</FooterHeading>
+
+            <p>
+              Library Privacy Statement
+              <br />
+              Except where otherwise noted, this work is subject to a Creative
+              Commons Attribution 4.0 license. For details and exceptions, see
+              the Library Copyright Policy.
+            </p>
+          </section>
         </div>
       </Margins>
+      <div
+        css={{
+          color: COLORS['blue']['200'],
+          background: COLORS['blue']['500'],
+          padding: `${SPACING['XS']} 0`,
+        }}
+      >
+        <Margins>
+          <p>©{year}, Regents of the University of Michigan</p>
+        </Margins>
+      </div>
     </footer>
   )
 }
