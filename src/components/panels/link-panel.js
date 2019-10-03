a7ca74982c6e3b2748327a92de7869ef23abe8ee
Jon Earley <jonathan.a.earley@gmail.com>
2019-10-03 08:56:33 -0400

    Move view all databases link under list.

--- src/components/panels/link-panel.js
+++ src/components/panels/link-panel.js
@@ -33,8 +33,9 @@ export default function LinkPanel({ data }) {
               <Link kind="list" to={d.uri}>{d.title}</Link>
             </li>
           ))}
-          <li><Link kind="list-strong" to="https://search.lib.umich.edu/databases/browse">View all databases</Link></li>
         </ol>
+
+        <Link kind="list-strong" to="https://search.lib.umich.edu/databases/browse">View all databases</Link>
       </section>
     )
   }
