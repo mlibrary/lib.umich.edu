a03a2199b3f861e580d9d7f5f6f8ccf70a1ea970
Jon Earley <jonathan.a.earley@gmail.com>
2019-10-28 20:50:50 -0400

    Adjust the site map.

--- src/pages/site-map.js
+++ src/pages/site-map.js
@@ -6,7 +6,8 @@ import {
   Text,
   SPACING,
   Margins,
-  COLORS
+  COLORS,
+  MEDIA_QUERIES
 } from '@umich-lib/core'
 import SEO from '../components/seo'
 import Breadcrumb from '../components/breadcrumb'
@@ -34,7 +35,13 @@ export default function SiteMap() {
       <SEO title="Site map" />
       <Margins css={{
         'ol ol': {
-          borderLeft: `solid 1px ${COLORS['neutral']['200']}`
+          borderLeft: `solid 1px ${COLORS['blue']['200']}`
+        },
+        '> ol': {
+          [MEDIA_QUERIES.LARGESCREEN]: {
+            columns: '2',
+          },
+          breakInside: 'avoid',
         },
         marginBottom: SPACING['4XL']
       }}>
@@ -43,12 +50,12 @@ export default function SiteMap() {
           marginBottom: SPACING['L']
         }}>Site map</Heading>
 
-        <Heading size="L" level={2} css={{
+        <Heading size="M" level={2} css={{
           marginTop: SPACING['XL']
         }}>Utility navigation</Heading>
         <NestLinkedList data={secondary} />
 
-        <Heading size="L" level={2} css={{
+        <Heading size="M" level={2} css={{
           marginTop: SPACING['XL']
         }}>Main navigation</Heading>
         <NestLinkedList data={primary} />
@@ -64,15 +71,17 @@ function NestLinkedList({ data }) {
       listStylePosition: 'inside', 
       'li li': {
         marginLeft: SPACING['XL']
+      },
+      '> li': {
+        breakInside: 'avoid'
       }
     }}>
       {data.map(({ text, to, children }) => (
-        <li key={to}>
-          <Link to={to} css={{
-            display: 'inline-block',
-            margin: SPACING['S'],
-            marginLeft: '0'
-          }}>{text}</Link>
+        <li key={to} css={{
+          margin: SPACING['S'],
+          marginLeft: '0'
+        }}>
+          <Link to={to}>{text}</Link>
           {children && (<NestLinkedList data={children} />)}
         </li>
       ))}
