468e0d235f99f3fbc6973361ab0e357a9e420b7f
Jon Earley <jonathan.a.earley@gmail.com>
2019-10-28 20:33:04 -0400

    Add site map page -- LIBWEB-496.

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,131 +12,95 @@ import {
 import Header from './header'
 import Footer from './footer'
 import DevelopmentAlert from './development-alert'
+import useNavigationData from '../hooks/use-navigation-data'
 
-const Layout = ({ children, drupalNid }) => (
-  <StaticQuery
-    query={graphql`
-      {
-        allNavPrimary {
-          edges {
-            node {
-              nav {
-                to
-                text
-                children {
-                  text
-                  to
-                  children {
-                    text
-                    to
-                  }
-                }
-              }
-            }
-          }
-        }
-        allNavUtility {
-          edges {
-            node {
-              nav {
-                to
-                text
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      const primary = data.allNavPrimary.edges[0].node.nav
-      const secondary = data.allNavUtility.edges[0].node.nav
+function Layout({ children, drupalNid }) {
+  const { primary, secondary } = useNavigationData()
 
-      return (
-        <React.Fragment>
-          <GlobalStyleSheet />
-          <Global
-            styles={{
-              'html, body, #___gatsby, #___gatsby > div': {
-                height: '100%',
+  return (
+    <React.Fragment>
+      <GlobalStyleSheet />
+      <Global
+        styles={{
+          'html, body, #___gatsby, #___gatsby > div': {
+            height: '100%',
+          },
+          '*:focus': {
+            outlineColor: COLORS.blue['400'],
+          },
+        }}
+      />
+      <div
+        css={{
+          minHeight: '100%',
+          display: 'grid',
+          gridTemplateRows: 'auto 1fr auto',
+          gridTemplateColumns: '100%',
+        }}
+      >
+        <section
+          aria-label="Skip links"
+          css={{
+            background: COLORS['blue']['400'],
+            ':focus-within': {
+              padding: `${SPACING['M']} 0`,
+              position: 'static',
+              width: 'auto',
+              height: 'auto',
+              a: {
+                color: 'white',
+                textDecoration: 'underline',
+                padding: SPACING['XS'],
               },
-              '*:focus': {
-                outlineColor: COLORS.blue['400'],
+              'ul > li:first-of-type': {
+                marginBottom: SPACING['M'],
               },
-            }}
-          />
-          <div
-            css={{
-              minHeight: '100%',
-              display: 'grid',
-              gridTemplateRows: 'auto 1fr auto',
-              gridTemplateColumns: '100%',
-            }}
-          >
-            <section
-              aria-label="Skip links"
-              css={{
-                background: COLORS['blue']['400'],
-                ':focus-within': {
-                  padding: `${SPACING['M']} 0`,
-                  position: 'static',
-                  width: 'auto',
-                  height: 'auto',
-                  a: {
-                    color: 'white',
-                    textDecoration: 'underline',
-                    padding: SPACING['XS'],
-                  },
-                  'ul > li:first-of-type': {
-                    marginBottom: SPACING['M'],
-                  },
-                  li: {
-                    textAlign: 'center',
-                  },
-                },
-                position: 'absolute',
-                left: '-10000px',
-                top: 'auto',
-                width: '1px',
-                height: '1px',
-                overflow: 'hidden',
-                '*:focus': {
-                  outlineColor: 'white',
-                },
-              }}
-            >
-              <Margins>
-                <ul>
-                  <li>
-                    <a href="#maincontent">Skip to content</a>
-                  </li>
-                  <li>
-                    <a href="#ask-a-librarian-chat">
-                      Skip to Ask a Librarian chat
-                    </a>
-                  </li>
-                </ul>
-              </Margins>
-            </section>
-            <section>
-              <DevelopmentAlert />
-              <UniversalHeader />
-              <Header primary={primary} secondary={secondary} />
-            </section>
-            <main id="maincontent">{children}</main>
-            <Footer />
-          </div>
-          {drupalNid && (
-            <div
-              css={{ display: 'none' }}
-              id="drupalNid"
-              data-drupal-nid={drupalNid}
-            />
-          )}
-        </React.Fragment>
-      )
-    }}
-  />
-)
+              li: {
+                textAlign: 'center',
+              },
+            },
+            position: 'absolute',
+            left: '-10000px',
+            top: 'auto',
+            width: '1px',
+            height: '1px',
+            overflow: 'hidden',
+            '*:focus': {
+              outlineColor: 'white',
+            },
+          }}
+        >
+          <Margins>
+            <ul>
+              <li>
+                <a href="#maincontent">Skip to content</a>
+              </li>
+              <li>
+                <a href="#ask-a-librarian-chat">
+                  Skip to Ask a Librarian chat
+                </a>
+              </li>
+            </ul>
+          </Margins>
+        </section>
+        <section>
+          <DevelopmentAlert />
+          <UniversalHeader />
+          <Header primary={primary} secondary={secondary} />
+        </section>
+        <main id="maincontent">{children}</main>
+        <Footer />
+      </div>
+      {drupalNid && (
+        <div
+          css={{ display: 'none' }}
+          id="drupalNid"
+          data-drupal-nid={drupalNid}
+        />
+      )}
+    </React.Fragment>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
