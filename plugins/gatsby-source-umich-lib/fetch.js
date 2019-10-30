d1e5ea47d1ed84f62e61bf87cac90d6bde0d716c
Jon Earley <jonathan.a.earley@gmail.com>
2019-10-30 12:42:42 -0400

    Reduce limiter.

--- plugins/gatsby-source-umich-lib/fetch.js
+++ plugins/gatsby-source-umich-lib/fetch.js
@@ -1,21 +1,21 @@
-const fetch = require('fetch-retry');
-const Bottleneck = require("bottleneck/es5");
+const fetch = require('fetch-retry')
+const Bottleneck = require('bottleneck/es5')
 
 function fetchWithRetry(url) {
   return fetch(url, {
     retries: 5,
     retryOn: [500],
     retryDelay: function(attempt) {
-      return Math.pow(2, attempt) * 1000; // 1000, 2000, 4000
-    }
+      return Math.pow(2, attempt) * 1000 // 1000, 2000, 4000
+    },
   }).then(function(response) {
-    return response.json();
+    return response.json()
   })
 }
 
 const limiter = new Bottleneck({
-  maxConcurrent: 40
-});
+  maxConcurrent: 25,
+})
 
 const fetchWithRetryAndLimited = limiter.wrap(fetchWithRetry)
 
