780de236b3d32a205af41cae6a31197af928eb8e
Jon Earley <jonathan.a.earley@gmail.com>
2019-11-11 13:53:26 -0500

    Setup for callout style.

---
+++ src/maybe-design-system/callout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {
+  SPACING,
+  COLORS,
+} from '@umich-lib/core'
+
+export default function Callout({ children, ...rest }) {
+  return (
+    <div
+      css={{
+        margin: `${SPACING['XL']} 0`,
+        padding: SPACING['XL'],
+        border: `solid 1px ${COLORS.neutral['100']}`,
+        borderLeft: `solid 4px ${COLORS.maize['400']}`,
+        borderRadius: '4px',
+        maxWidth: '38rem'
+      }}
+      data-umich-lib-callout
+    >
+      {children}
+    </div>
+  )
+}
