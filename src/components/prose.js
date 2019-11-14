4c837c0a58397fbd0129aac17095223191673ec0
Jon Earley <jonathan.a.earley@gmail.com>
2019-11-14 10:30:36 -0500

    Reduce and simplify callout spacings.

--- src/components/prose.js
+++ src/components/prose.js
@@ -1,36 +1,28 @@
 import styled from '@emotion/styled'
-import {
-  SPACING
-} from '@umich-lib/core'
+import { SPACING } from '@umich-lib/core'
 
-const Prose = styled("div")({
-  "> *:not(:last-child)": {
-    marginBottom: SPACING["M"]
+const Prose = styled('div')({
+  '> *:not(:last-child)': {
+    marginBottom: SPACING['M'],
   },
-  "> h2:not(:last-child)": {
-    marginTop: SPACING["2XL"]
+  '> h2:not(:last-child)': {
+    marginTop: SPACING['2XL'],
   },
-  "> h3:not(:last-child)": {
-    marginTop: SPACING["M"]
+  '> h3:not(:last-child)': {
+    marginTop: SPACING['M'],
   },
-  "> h4:not(:last-child)": {
-    marginTop: SPACING["M"]
+  '> h4:not(:last-child)': {
+    marginTop: SPACING['M'],
   },
-  "> h5:not(:last-child)": {
-    marginTop: SPACING["M"]
+  '> h5:not(:last-child)': {
+    marginTop: SPACING['M'],
   },
   li: {
-    marginBottom: SPACING["XS"]
-  },
-  '[data-umich-lib-callout]': {
-    margin: `${SPACING['3XL']} 0`,
+    marginBottom: SPACING['XS'],
   },
   '[data-umich-lib-callout] + [data-umich-lib-callout]': {
-    marginTop: SPACING['L']
+    marginTop: SPACING['L'],
   },
-  '[data-umich-lib-callout] + h2, [data-umich-lib-callout] + h3, [data-umich-lib-callout] + h4': {
-    marginTop: SPACING['4XL']
-  }
-});
+})
 
 export default Prose
--- src/maybe-design-system/callout.js
+++ src/maybe-design-system/callout.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
-import {
-  SPACING,
-  COLORS,
-} from '@umich-lib/core'
+import { SPACING, COLORS } from '@umich-lib/core'
 
 export default function Callout({ children, ...rest }) {
   return (
     <div
       css={{
         margin: `${SPACING['XL']} 0`,
-        padding: SPACING['XL'],
+        padding: SPACING['L'],
         border: `solid 1px ${COLORS.neutral['100']}`,
         borderLeft: `solid 4px ${COLORS.maize['400']}`,
         borderRadius: '2px',
-        maxWidth: '38rem'
+        maxWidth: '38rem',
       }}
       data-umich-lib-callout
     >
