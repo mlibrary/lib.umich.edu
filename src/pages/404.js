68f6e196577fd6fe53018d1bcf684d542e737fe3
Jon Earley <Jonathan.a.earley@gmail.com>
2019-11-14 06:52:48 -0800

    Setup for updated 404 page with flowing image - LIBWEB-526. (#33)
    Updated 404 page with responsive image.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,97 @@
 import React from 'react'
-import Layout from '../components/layout'
+import { useStaticQuery, graphql } from "gatsby"
 
 import {
   Heading,
   Text,
   SPACING,
   Margins,
-  COLORS
+  COLORS,
+  List
 } from '@umich-lib/core'
+import Img from 'gatsby-image'
 import SEO from '../components/seo'
+import Link from '../components/link'
+import Prose from '../components/prose'
+import Layout from '../components/layout'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404" />
-    <Margins css={{
-      marginTop: SPACING['4XL'],
-      marginBottom: SPACING['4XL'],
-    }}>
-      <Heading size="3XL" level="1" css={{ marginBottom: SPACING['M'] }}><span
-        css={{
-          fontSize: '60%',
-          display: 'block',
-          color: COLORS.orange['400']
-        }}>404</span>Page not found</Heading>
-      <Text lede>We are unable to find this page.</Text>
-    </Margins>
-  </Layout>
-)
+import MEDIA_QUERIES from '../maybe-design-system/media-queries'
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO title="404 - Page not found" />
+      <Margins css={{
+        marginTop: SPACING['XL'],
+        [MEDIA_QUERIES['M']]: {
+          marginTop: SPACING['3XL']
+        }
+      }}>
+        <Image />
+        <Heading size="3XL" level="1" css={{ marginBottom: SPACING['M'] }}>
+          <span
+          css={{
+            fontSize: '60%',
+            display: 'block',
+            color: COLORS.orange['400']
+          }}>404
+          </span>Page not found</Heading>
+        <Prose css={{
+          marginBottom: '8rem'
+        }}>
+          <Text lede>We can’t find the page you’re looking for. This can sometimes happen when:</Text>
+
+          <List type="bulleted">
+            <li>We’ve moved or renamed the page</li>
+            <li>The page no longer exists</li>
+            <li>There’s a slight typo in the web address </li>
+            <li>The web address wasn’t copied fully</li>
+          </List>
+
+          <Text>If the web address is correct, please contact us at <Link to="/ask-librarian">Ask a Librarian</Link> and we’ll do our best to find what you’re looking for.</Text>
+
+          <Text>You can also visit the <Link to="/">homepage</Link>, navigate through our menu options, or use site search.</Text>
+        </Prose>
+      </Margins>
+    </Layout>
+  )
+}
+
+function Image() {
+  const imageData = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "404.png" }) {
+        childImageSharp {
+          fluid(quality: 80, maxWidth: 1280) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div 
+      css={{
+        display: 'block',
+        [MEDIA_QUERIES['M']]: {
+          margin: SPACING['L'],
+          float: 'right',
+          width: '40vw',
+          shapeMargin: SPACING['XL'],
+          shapeOutside: 'polygon(65px 381px, 84px 210px, -18px 211px, -35px 141px, -11px 106px, 103px 115px, 123px 96px, 189px 111px, 262px 27px, 266px -14px, 313px -30px, 427px -29px, 475px 18px, 456px 75px, 458px 177px, 504px 163px, 632px 171px, 632px 225px, 536px 235px, 524px 490px, 44px 491px)',
+          marginBottom: 0
+        },
+        [MEDIA_QUERIES['L']]: {
+          width: '50vw',
+          maxWidth: '38rem',
+        },
+        marginBottom: SPACING['2XL']
+      }}
+    >
+      <Img fluid={imageData.file.childImageSharp.fluid} alt="" />
+    </div>
+  )
+}
 
 export default NotFoundPage
--- src/pages/search.js
+++ src/pages/search.js
@@ -25,8 +25,6 @@ export default function PlaceholderSearchResultsPage() {
     }
   `)
 
-  console.log('imageData', imageData)
-
   return (
     <Layout>
       <SEO title="Search results" />
