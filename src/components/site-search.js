21e36d006ed129cdfb9a03af3a9cb389bb9ccec1
Jon Earley <jonathan.a.earley@gmail.com>
2019-11-26 10:48:03 -0500

    Let site search server side render.

--- src/components/site-search.js
+++ src/components/site-search.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { StaticQuery, graphql, navigate, Link } from 'gatsby'
 import { Index } from 'elasticlunr'
-import { SPACING, Z_SPACE, COLORS } from '@umich-lib/core'
+import { SPACING, Z_SPACE, COLORS, Icon } from '@umich-lib/core'
 import {
   Combobox,
   ComboboxInput,
@@ -13,16 +13,6 @@ import {
 import '@reach/dialog/styles.css'
 
 export default function SiteSearchWrapper() {
-  const [initialized, setInitialized] = useState(false)
-
-  useEffect(() => {
-    setInitialized(true)
-  }, [])
-
-  if (!initialized) {
-    return null
-  }
-
   return (
     <StaticQuery
       query={graphql`
@@ -60,92 +50,107 @@ function SiteSearch({ siteIndex }) {
           alignItems: 'center',
         }}
       >
+        <Icon
+          icon="search"
+          size={24}
+          css={{
+            marginRight: '-2rem',
+          }}
+        />
         <ComboboxInput
           aria-label="Search this site"
           onChange={handleChange}
           placeholder="Search this site"
+          type="search"
+          autoComplete="off"
+          autocomplete={false}
           css={{
             fontSize: '1rem',
-            border: 'solid 1px rgba(0,0,0,0.5)',
-            boxShadow: 'inset 0 1px 4px rgba(0,0,0,0.1)',
-            borderRadius: '4px',
-            padding: `${SPACING['XS']} ${SPACING['S']}`,
-            width: '100%',
             appearance: 'textfield',
             fontFamily: 'inherit',
             boxSizing: 'border-box',
+            width: '100%',
+            background: 'none',
+            borderRadius: '2px',
+            padding: SPACING['XS'],
+            paddingLeft: '2.1rem',
+            border: `solid 1px ${COLORS.neutral['300']}`,
+            alignItems: 'center',
+            boxShadow: `inset 0 1px 4px rgba(0,0,0,0.1)`,
           }}
-          type="search"
-          autoComplete="off"
         />
-      </div>
-      {results && (
-        <ComboboxPopover
-          css={{
-            position: 'relative',
-          }}
-        >
-          <ComboboxList
-            persistSelection
-            aria-label="Results"
-            css={{
-              position: 'absolute',
-              top: '0',
-              zIndex: '999',
-              width: '100%',
-              background: 'white',
-              ...Z_SPACE['16'],
-              '[aria-selected="true"]': {
-                background: COLORS.teal['400'],
-                color: 'white',
-                fontWeight: '700',
-                '[data-user-value]': {
-                  background: 'transparent',
+        <ComboboxPopover>
+          {results && (
+            <ComboboxList
+              persistSelection
+              aria-label="Results"
+              css={{
+                position: 'absolute',
+                top: '0',
+                left: 0,
+                zIndex: '999',
+                width: '100%',
+                background: 'white',
+                ...Z_SPACE['16'],
+                '[aria-selected="true"]': {
+                  background: COLORS.teal['400'],
+                  color: 'white',
+                  fontWeight: '700',
+                  '[data-user-value]': {
+                    background: 'transparent',
+                  },
                 },
-              },
-              '[data-reach-combobox-option]:not(:last-of-type)': {
-                borderBottom: `solid 1px ${COLORS.neutral['100']}`,
-              },
-            }}
-          >
-            {results.length === 0 && (
+                '[data-reach-combobox-option]:not(:last-of-type)': {
+                  borderBottom: `solid 1px ${COLORS.neutral['100']}`,
+                },
+              }}
+            >
               <p
                 css={{
                   padding: SPACING['S'],
                   color: COLORS.neutral['300'],
+                  background: COLORS.blue['100'],
+                  borderBottom: `solid 1px`,
+                  borderBottomColor: COLORS.neutral['100'],
                 }}
               >
-                No results found.
+                {results.length === 0 ? (
+                  'No results found'
+                ) : (
+                  <span css={{ fontSize: '0.875rem' }}>
+                    ↑↓ to navigate, enter to select, esc to dismiss
+                  </span>
+                )}
               </p>
-            )}
-            {results.slice(0, 10).map((result, index) => (
-              <ComboboxOption
-                key={index}
-                value={result.title}
-                css={{
-                  '[data-user-value]': {
-                    fontWeight: '700',
-                    background: COLORS.maize['200'],
-                  },
-                }}
-              >
-                <Link
-                  to={result.slug}
+              {results.slice(0, 10).map((result, index) => (
+                <ComboboxOption
+                  key={index}
+                  value={result.title}
                   css={{
-                    display: 'block',
-                    padding: SPACING['S'],
-                    ':hover': {
-                      textDecoration: 'underline',
+                    '[data-user-value]': {
+                      fontWeight: '700',
+                      background: COLORS.maize['200'],
                     },
                   }}
                 >
-                  <ComboboxOptionText />
-                </Link>
-              </ComboboxOption>
-            ))}
-          </ComboboxList>
+                  <Link
+                    to={result.slug}
+                    css={{
+                      display: 'block',
+                      padding: SPACING['S'],
+                      ':hover': {
+                        textDecoration: 'underline',
+                      },
+                    }}
+                  >
+                    <ComboboxOptionText />
+                  </Link>
+                </ComboboxOption>
+              ))}
+            </ComboboxList>
+          )}
         </ComboboxPopover>
-      )}
+      </div>
     </Combobox>
   )
 }
