4fa604176cdf530abf60c80bcb6a283f99049c16
Jon Earley <jonathan.a.earley@gmail.com>
2019-11-26 15:13:29 -0500

    Adjusting breakpoints for small screen search modal.

--- src/components/site-search-modal.js
+++ src/components/site-search-modal.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Dialog } from '@reach/dialog'
-import { SPACING } from '@umich-lib/core'
+import { SPACING, Z_SPACE } from '@umich-lib/core'
 
 import SiteSearch from './site-search'
+import MEDIA_QUERIES from '../maybe-design-system/media-queries'
 
 function SiteSearchModal({ handleDismiss }) {
   return (
@@ -12,6 +13,13 @@ function SiteSearchModal({ handleDismiss }) {
         width: '100%',
         padding: SPACING['M'],
         margin: '0',
+        ...Z_SPACE['16'],
+        [MEDIA_QUERIES['M']]: {
+          borderRadius: '2px',
+          width: '67vw',
+          padding: SPACING['L'],
+          margin: '9vh auto',
+        },
       }}
     >
       <SiteSearch />
