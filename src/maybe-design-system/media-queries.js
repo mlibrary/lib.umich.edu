375052953c8f0f2521b2d714e11a6e28663129d6
Jon Earley <jonathan.a.earley@gmail.com>
2019-12-17 11:58:45 -0500

    First pass at small screen hours view.

--- src/maybe-design-system/media-queries.js
+++ src/maybe-design-system/media-queries.js
@@ -1,10 +1,10 @@
 import { MEDIA_QUERIES as MQ } from '@umich-lib/core'
 
 const MEDIA_QUERIES = {
-  'XL': '@media only screen and (min-width: 1200px)',
-  'L': '@media only screen and (min-width: 920px)',
-  'M': '@media only screen and (min-width: 720px)',
-  'S': MQ.LARGESCREEN
+  XL: '(min-width: 1200px)',
+  L: '(min-width: 920px)',
+  M: '(min-width: 720px)',
+  S: MQ.LARGESCREEN,
 }
 
 export default MEDIA_QUERIES
