b0840378e95d7b438d137bf119f65d238a3de400
Jon Earley <Jonathan.a.earley@gmail.com>
2020-03-25 09:57:50 -0400

    Bugfix: Let users click through the icon to interact with the select box

--- src/templates/staff-directory.js
+++ src/templates/staff-directory.js
@@ -604,6 +604,7 @@ function Select({ label, name, options, value, ...rest }) {
             position: 'absolute',
             right: SPACING['S'],
             bottom: SPACING['S'],
+            pointerEvents: 'none'
           }}
         />
       </div>
