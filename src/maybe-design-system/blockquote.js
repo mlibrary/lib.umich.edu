34bb6fcbc7010d2b5963ca3ce4b07433744c2a1d
Jon Earley <jonathan.a.earley@gmail.com>
2020-04-29 08:52:08 -0400

    Make blockquote border teal -- LIBWEB-685

--- src/maybe-design-system/blockquote.js
+++ src/maybe-design-system/blockquote.js
@@ -9,7 +9,7 @@ export default function Blockquote({ children, ...rest }) {
         ...TYPOGRAPHY['XS'],
         margin: SPACING['XL'] + '!important',
         paddingLeft: SPACING['M'],
-        borderLeft: `solid 4px ${COLORS.maize['400']}`,
+        borderLeft: `solid 4px ${COLORS.teal['400']}`,
         maxWidth: '38rem',
         fontStyle: 'italic',
       }}
