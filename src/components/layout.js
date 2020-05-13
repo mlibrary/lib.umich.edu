3e53846cab0afcffd7dfc5ad0715024f4a743918
Jon Earley <jonathan.a.earley@gmail.com>
2020-05-13 13:31:23 -0400

    Move UH and Library Alerts to gatsby-browser.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Global } from '@emotion/core'
-import { UniversalHeader, GlobalStyleSheet, COLORS } from '@umich-lib/core'
+import { GlobalStyleSheet, COLORS } from '@umich-lib/core'
 import Header from './header'
 import Footer from './footer'
-import DevelopmentAlert from './development-alert'
-import SkipLinks from './skip-links'
 import useNavigationData from '../hooks/use-navigation-data'
-import LibraryAlerts from '../maybe-design-system/library-alerts'
 
 function Layout({ children, drupalNid }) {
   const { primary, secondary } = useNavigationData()
@@ -42,24 +39,9 @@ function Layout({ children, drupalNid }) {
           },
         }}
       />
-      <div
-        css={{
-          minHeight: '100%',
-          display: 'grid',
-          gridTemplateRows: 'auto 1fr auto',
-          gridTemplateColumns: '100%',
-        }}
-      >
-        <section>
-          <SkipLinks />
-          <DevelopmentAlert />
-          <UniversalHeader />
-          <LibraryAlerts domain="lib.umich.edu" />
-          <Header primary={primary} secondary={secondary} />
-        </section>
-        <main id="maincontent">{children}</main>
-        <Footer />
-      </div>
+      <Header primary={primary} secondary={secondary} />
+      <main id="maincontent">{children}</main>
+      <Footer />
       {drupalNid && (
         <div
           css={{ display: 'none' }}
--- src/maybe-design-system/library-alerts.js
+++ src/maybe-design-system/library-alerts.js
@@ -3,74 +3,84 @@ import { Margins, COLORS, SPACING, Icon } from '@umich-lib/core'
 import Alert from '@reach/alert'
 
 export default function LibraryAlerts({ domain }) {
+  const [status, setStatus] = useState('loading')
   const [alerts, setAlerts] = useState(null)
 
   async function fetchAlerts() {
-    const response = await fetch('https://staff.lib.umich.edu/api/alerts')
+    try {
+      const response = await fetch('https://staff.lib.umich.edu/api/alerts')
 
-    if (response.status === 200) {
-      const result = await response.json()
-      const filtered = result.reduce((memo, alert) => {
-        const domains = alert.domains.split(', ')
+      if (response.status === 200) {
+        const result = await response.json()
+        const filtered = result.reduce((memo, alert) => {
+          const domains = alert.domains.split(', ')
 
-        if (domains.includes(domain)) {
-          memo = memo.concat(alert)
-        }
+          if (domains.includes(domain)) {
+            memo = memo.concat(alert)
+          }
 
-        return memo
-      }, [])
+          return memo
+        }, [])
 
-      setAlerts(filtered)
+        setAlerts(filtered)
+        setStatus('success')
+      }
+    } catch {
+      console.warn('Unable to fetch U-M Library Website Alerts.')
+      setStatus('error')
     }
   }
 
   useEffect(() => {
-    fetchAlerts()
-  })
+    if (status === 'loading') {
+      fetchAlerts()
+    }
+  }, [status])
 
-  if (!alerts) {
-    return null
-  }
-
-  return (
-    <React.Fragment>
-      {alerts.map((alert, i) => (
-        <div
-          css={{
-            background: COLORS.orange['300'],
-            padding: `${SPACING['XS']} 0`,
-            a: {
-              textDecoration: 'underline',
-            },
-          }}
-        >
-          <Margins>
-            <div
-              css={{
-                display: 'grid',
-                gridTemplateColumns: 'auto 1fr',
-                gridGap: SPACING['S'],
-                br: {
-                  display: 'none',
-                },
-                'strong, b': {
-                  fontWeight: '800',
-                },
-              }}
-            >
-              <Icon icon="warning" css={{ marginTop: '6px' }} />{' '}
-              <Alert
+  if (status === 'success') {
+    return (
+      <React.Fragment>
+        {alerts.map((alert, i) => (
+          <div
+            key={alert.uuid}
+            css={{
+              background: COLORS.orange['300'],
+              padding: `${SPACING['XS']} 0`,
+              a: {
+                textDecoration: 'underline',
+              },
+            }}
+          >
+            <Margins>
+              <div
                 css={{
-                  '* + *': {
-                    marginTop: SPACING['S'],
+                  display: 'grid',
+                  gridTemplateColumns: 'auto 1fr',
+                  gridGap: SPACING['S'],
+                  br: {
+                    display: 'none',
+                  },
+                  'strong, b': {
+                    fontWeight: '800',
                   },
                 }}
-                dangerouslySetInnerHTML={{ __html: alert.description }}
-              />
-            </div>
-          </Margins>
-        </div>
-      ))}
-    </React.Fragment>
-  )
+              >
+                <Icon icon="warning" css={{ marginTop: '6px' }} />{' '}
+                <Alert
+                  css={{
+                    '* + *': {
+                      marginTop: SPACING['S'],
+                    },
+                  }}
+                  dangerouslySetInnerHTML={{ __html: alert.description }}
+                />
+              </div>
+            </Margins>
+          </div>
+        ))}
+      </React.Fragment>
+    )
+  }
+
+  return null
 }
