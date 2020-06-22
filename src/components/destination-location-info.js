464a332d91d26413311d6ee7cbe270d96282dfb9
Jon Earley <jonathan.a.earley@gmail.com>
2020-06-22 11:27:42 -0400

    Make location info for destination pages optional.

--- src/components/destination-location-info.js
+++ src/components/destination-location-info.js
@@ -23,19 +23,19 @@ function DestinationLocationInfo({ node }) {
   const { field_parent_location, field_room_building } = node.relationships
   const bid = field_room_building
     ? field_room_building.id
-    : node.relationships.field_parent_location.id
-  const fid = node.relationships.field_floor.id
+    : field_parent_location?.id
+  const fid = node.relationships.field_floor?.id
   const floorPlan = useFloorPlan(bid, fid)
   const shouldDisplayHours = node.field_display_hours_
   const locationTitle = field_parent_location
-    ? field_parent_location.title
-    : field_room_building.title
+    ? field_parent_location?.title
+    : field_room_building?.title
   const phone_number = node.field_phone_number
   const email = node.field_email
   const floor = getFloor({ node })
   const room = node.field_room_number && 'Room ' + node.field_room_number
   const locationSummary = [locationTitle, floor, room]
-    .filter(i => i !== undefined)
+    .filter(i => typeof i === 'string')
     .join(', ')
 
   return (
@@ -61,16 +61,18 @@ function DestinationLocationInfo({ node }) {
           </IconText>
         </p>
       )}
-      <p>
-        <IconText d={icons['address']}>
-          <span>
-            {locationSummary}
-            <span css={{ display: 'block' }}>
-              <Link to={floorPlan.fields.slug}>View floorplan</Link>
+      {floorPlan && (
+        <p>
+          <IconText d={icons['address']}>
+            <span>
+              {locationSummary}
+              <span css={{ display: 'block' }}>
+                <Link to={floorPlan.fields.slug}>View floorplan</Link>
+              </span>
             </span>
-          </span>
-        </IconText>
-      </p>
+          </IconText>
+        </p>
+      )}
       {phone_number && (
         <p>
           <IconText d={icons['phone']}>
