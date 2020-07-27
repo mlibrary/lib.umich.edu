e901aa11055fcdf093a96e538da108563b472f82
Jon Earley <jonathan.a.earley@gmail.com>
2020-07-27 16:20:58 -0400

    Set dev env to use host and sitemap null.

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,8 @@ module.exports = {
               {
                 userAgent: '*',
                 disallow: ['/'],
+                host: null,
+                sitemap: null,
               },
             ],
           },
