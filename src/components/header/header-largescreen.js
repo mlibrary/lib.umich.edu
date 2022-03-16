2384283b39c892882c8f41a4802aadeff8147a9d
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-03-16 14:17:56 -0400

    Replacing anonymous function - Assign arrow function to a variable before exporting as module default - import/no-anonymous-default-export

--- src/components/header/header-largescreen.js
+++ src/components/header/header-largescreen.js
@@ -6,92 +6,96 @@ import Nav from './primary-nav'
 import Logo from './logo'
 import SiteSearch from '../site-search'
 
-export default ({ primary, secondary }) => (
-  <header
-    css={{
-      borderBottom: `solid 2px ${COLORS.neutral[100]}`,
-      display: 'block',
-      '@media only screen and (max-width: 1128px)': {
-        display: 'none',
-      },
-    }}
-  >
-    <Margins>
-      <div
-        css={{
-          paddingTop: SPACING['M'],
-          position: 'relative',
-        }}
-      >
-        <Logo size={36} />
-
+function HeaderLargeScreen({ primary, secondary }) {
+  return (
+    <header
+      css={{
+        borderBottom: `solid 2px ${COLORS.neutral[100]}`,
+        display: 'block',
+        '@media only screen and (max-width: 1128px)': {
+          display: 'none',
+        },
+      }}
+    >
+      <Margins>
         <div
           css={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'flex-end',
-            marginTop: SPACING['XS'],
+            paddingTop: SPACING['M'],
+            position: 'relative',
           }}
         >
-          <Nav items={primary} />
-          {secondary && (
-            <nav
-              css={{
-                position: 'absolute',
-                top: SPACING['M'],
-                right: '0',
-              }}
-              aria-label="Utility"
-            >
-              <ul>
-                {secondary.map(({ text, to, icon }, i) => (
-                  <li
-                    css={{
-                      display: 'inline-block',
-                      ':not(:last-child)': {
-                        marginRight: SPACING['M'],
-                      },
-                    }}
-                    key={i + text}
-                  >
-                    <PlainLink
-                      to={to}
-                      external={to === '/my-account'}
-                      css={{
-                        ...LINK_STYLES['special'],
-                        padding: `${SPACING['S']} 0`,
-                        ':hover': {
-                          '.text': LINK_STYLES['special'][':hover'],
-                        },
-                      }}
-                    >
-                      {icon && (
-                        <Icon
-                          icon={icon}
-                          size={14}
-                          css={{
-                            marginRight: SPACING['2XS'],
-                            marginTop: '-2px',
-                          }}
-                        />
-                      )}
-                      <span className="text">{text}</span>
-                    </PlainLink>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          )}
+          <Logo size={36} />
+
           <div
             css={{
-              flexBasis: '21rem',
-              marginBottom: SPACING['M'],
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'flex-end',
+              marginTop: SPACING['XS'],
             }}
           >
-            <SiteSearch label="Search this site" />
+            <Nav items={primary} />
+            {secondary && (
+              <nav
+                css={{
+                  position: 'absolute',
+                  top: SPACING['M'],
+                  right: '0',
+                }}
+                aria-label="Utility"
+              >
+                <ul>
+                  {secondary.map(({ text, to, icon }, i) => (
+                    <li
+                      css={{
+                        display: 'inline-block',
+                        ':not(:last-child)': {
+                          marginRight: SPACING['M'],
+                        },
+                      }}
+                      key={i + text}
+                    >
+                      <PlainLink
+                        to={to}
+                        external={to === '/my-account'}
+                        css={{
+                          ...LINK_STYLES['special'],
+                          padding: `${SPACING['S']} 0`,
+                          ':hover': {
+                            '.text': LINK_STYLES['special'][':hover'],
+                          },
+                        }}
+                      >
+                        {icon && (
+                          <Icon
+                            icon={icon}
+                            size={14}
+                            css={{
+                              marginRight: SPACING['2XS'],
+                              marginTop: '-2px',
+                            }}
+                          />
+                        )}
+                        <span className="text">{text}</span>
+                      </PlainLink>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )}
+            <div
+              css={{
+                flexBasis: '21rem',
+                marginBottom: SPACING['M'],
+              }}
+            >
+              <SiteSearch label="Search this site" />
+            </div>
           </div>
         </div>
-      </div>
-    </Margins>
-  </header>
-)
+      </Margins>
+    </header>
+  )
+}
+
+export default HeaderLargeScreen
