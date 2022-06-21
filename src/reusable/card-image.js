d0f7456063d4220363e852b5a2a480aa34e4b323
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-06-21 13:59:59 -0400

    Adding `alt` backup if `undefined`

--- src/reusable/card-image.js
+++ src/reusable/card-image.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { COLORS, SPACING } from '@reusable';
 
@@ -6,7 +5,7 @@ export default function CardImage({ image }) {
   return (
     <GatsbyImage
       image={image}
-      alt={image.alt}
+      alt={image.alt || ''}
       css={{
         aspectRatio: '3 / 2',
         backgroundColor: COLORS.blue['100'],
