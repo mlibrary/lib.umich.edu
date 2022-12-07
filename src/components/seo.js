242cb8b4634ef67c143f4264d7c69a725af10b9a
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-12-07 11:01:43 -0500

    Replacing `StaticQuery` with `useStaticQuery` `StaticQuery` was being deprecated by `gatsby`

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 function SearchEngineOptimization({
   description,
@@ -10,66 +10,64 @@ function SearchEngineOptimization({
   keywords,
   title,
 }) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const metaDescription = description || data.site.siteMetadata.description;
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const metaDescription =
-          description || data.site.siteMetadata.description;
-
-        return (
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            defaultTitle={data.site.siteMetadata.title}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: 'description',
-                content: metaDescription,
-              },
-              {
-                property: 'og:title',
-                content: title,
-              },
-              {
-                property: 'og:description',
-                content: metaDescription,
-              },
-              {
-                property: 'og:type',
-                content: 'website',
-              },
-              {
-                name: 'twitter:description',
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords
-                  ? {
-                      name: 'keywords',
-                      content: keywords,
-                    }
-                  : []
-              )
-              .concat(meta)}
-          >
-            <link
-              href="https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css"
-              rel="stylesheet"
-            />
-            <script
-              async
-              type="text/javascript"
-              src="https://umich.edu/apis/umalerts/umalerts.js"
-            ></script>
-          </Helmet>
-        );
-      }}
-    />
+    <Helmet
+      htmlAttributes={{lang}}
+      defaultTitle={data.site.siteMetadata.title}
+      title={title}
+      titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:description',
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          keywords
+            ? {
+                name: 'keywords',
+                content: keywords,
+              }
+            : []
+        )
+        .concat(meta)}
+    >
+      <link
+        href="https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css"
+        rel="stylesheet"
+      />
+      <script
+        async
+        type="text/javascript"
+        src="https://umich.edu/apis/umalerts/umalerts.js"
+      ></script>
+    </Helmet>
   );
 }
 
@@ -88,13 +86,3 @@ SearchEngineOptimization.propTypes = {
 };
 
 export default SearchEngineOptimization;
-
-const detailsQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
