e87051e16edbedf8422fcdd97206e7177114e9f2
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-01-30 10:47:44 -0500

    Replacing `@reach/dialog` with native HTML `dialog` - Uninstalled `@reach/dialog` which now allows developers to run `npm install` without `--legacy-peer-deps` - Added keybinding actions to escape out of the dialog - Added class to `body` to make it stop scrolling when dialog is open

---
+++ src/components/header/site-search-modal.js
@@ -0,0 +1,76 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from 'react';
+import {
+  SPACING,
+  Icon,
+  Z_SPACE
+} from '../../reusable';
+import SiteSearch from '../site-search';
+
+function SiteSearchModal() {
+  return (
+    <>
+      <button
+        onClick={() => {
+          document.querySelector('body').classList.add('stop-scroll');
+          document.getElementById('dialog').showModal();
+        }}
+        css={{
+          padding: `${SPACING['M']} ${SPACING['XS']}`,
+        }}
+      >
+        <Icon icon="search" size={32} />
+        <span className='visually-hidden'>Search this site</span>
+      </button>
+      <dialog
+        id="dialog"
+        onClick={() => {
+          const dialog = document.getElementById('dialog');
+          dialog.addEventListener('click', ((event) => {
+            if(event.target === dialog) {
+              event.target.close();
+            }
+          }));
+        }}
+        onKeyDown={(event) => {
+          if(event.key === 'Escape') {
+            document.getElementById('dialog').close();
+          }
+        }}
+        onClose={() => {
+          document.querySelector('body').classList.remove('stop-scroll');
+        }}
+        css={{
+          border: '0',
+          borderRadius: '2px',
+          margin: `${SPACING['L']} auto`,
+          maxWidth: '82vw',
+          padding: '0',
+          width: '100%',
+          ...Z_SPACE['16'],
+          '::backdrop': {
+            backgroundColor: 'rgba(0, 0, 0, 0.6)'
+          },
+          '&[open]': {
+            display: 'table'
+          },
+          '& *[data-site-search-icon]': {
+            left: SPACING['L'],
+          },
+          '& input': {
+            border: 'none',
+            padding: '0.75rem 1.5rem 0.75rem 3.5rem'
+          },
+          '& .search-popover': {
+            maxWidth: '82vw',
+            position: 'fixed',
+          }
+        }}
+      >
+        <SiteSearch label="Search this site" css={{width: '100%'}} />
+      </dialog>
+    </>
+  );
+}
+
+export default SiteSearchModal;
