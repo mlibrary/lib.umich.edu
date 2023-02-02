4401104b7098f1eb5e28d667119234b88443776b
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-02-02 15:01:51 -0500

    Adding `head` components and updating `seo.js` - Added scripts and styles to `gatsby-ssr` to prevent component flickering from query parameter changes - Updated `SearchEngineOptimization` to only show `meta[name="keywords"]` when keywords exist

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,7 @@
-export const onRenderBody = ({ setHtmlAttributes }) => {
+export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en' });
+  setHeadComponents([
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css" />,
+    <script async type="text/javascript" src="https://umich.edu/apis/umalerts/umalerts.js"></script>
+  ]);
 };
