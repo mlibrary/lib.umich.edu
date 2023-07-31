951ba89a2cb6cee24206ca5d3c64315c2fa380c8
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2023-07-31 14:22:00 -0400

    eslint fixes, PropTypes, and multiple leaders
    This should clean the file up to match the .eslint rules. It also adds the PropTypes as part of the eslint requirements. Finally, this changes the leadership line to have formatting for multiple leaders.

--- src/templates/department.js
+++ src/templates/department.js
@@ -15,16 +15,17 @@ import LinkCallout from '../components/link-callout';
 import { stringifyState } from '../utils/get-url-state';
 import transformNodePanels from '../utils/transform-node-panels';
 import Panels from '../components/panels';
+import PropTypes from 'prop-types';
 
-function DepartmentTemplate({ data }) {
+function DepartmentTemplate ({ data }) {
   const node = data.department;
   const {
-    field_title_context,
+    field_title_context: fieldTitleContext,
     body,
     fields,
-    field_local_navigation,
-    field_make_an_appointment,
-    field_what_were_working_on,
+    field_local_navigation: fieldLocalNavigation,
+    field_make_an_appointment: fieldMakeAnAppointment,
+    field_what_were_working_on: fieldWhatWereWorkingOn
   } = node;
   const navBranch = useNavigationBranch(fields.slug);
   const smallScreenBranch = useNavigationBranch(fields.slug, 'small');
@@ -33,7 +34,7 @@ function DepartmentTemplate({ data }) {
     : null;
   const departmentInfo = processDepartmentInfo({
     node,
-    staffDirectorySlug: data.staffDirectoryNode.fields.slug,
+    staffDirectorySlug: data.staffDirectoryNode.fields.slug
   });
   const { bodyPanels, fullPanels } = transformNodePanels({ node });
 
@@ -45,14 +46,14 @@ function DepartmentTemplate({ data }) {
             <Breadcrumb data={fields.breadcrumb} />
           </Top>
           <Side>
-            {field_local_navigation && (
+            {fieldLocalNavigation && (
               <SideNavigation to={fields.slug} branch={navBranch} />
             )}
-            {field_local_navigation && smallScreenItems && (
+            {fieldLocalNavigation && smallScreenItems && (
               <SmallScreen>
                 <div
                   css={{
-                    margin: `0 -${SPACING['M']}`,
+                    margin: `0 -${SPACING.M}`
                   }}
                 >
                   <HorizontalNavigation items={smallScreenItems} />
@@ -62,65 +63,67 @@ function DepartmentTemplate({ data }) {
           </Side>
           <Content>
             <Heading
-              size="3XL"
+              size='3XL'
               level={1}
               css={{
-                marginBottom: SPACING['L'],
+                marginBottom: SPACING.L
               }}
             >
-              {field_title_context}
+              {fieldTitleContext}
             </Heading>
             <Prose>
-              <Heading size="M" level={2}>
+              <Heading size='M' level={2}>
                 Who we are
               </Heading>
 
               <List
                 css={{
                   svg: {
-                    marginRight: SPACING['XS'],
-                  },
+                    marginRight: SPACING.XS
+                  }
                 }}
               >
-                {departmentInfo.map(({ icon, d, content }, index) => (
-                  <li key={index}>
-                    {d ? <Icon d={d} /> : <Icon icon={icon} />}
-                    {content}
-                  </li>
-                ))}
+                {departmentInfo.map(({ icon, d, content }, index) => {
+                  return (
+                    <li key={index}>
+                      {d ? <Icon d={d} /> : <Icon icon={icon} />}
+                      {content}
+                    </li>
+                  );
+                })}
               </List>
 
-              {field_make_an_appointment && (
+              {fieldMakeAnAppointment && (
                 <div
                   css={{
                     marginTop: SPACING['2XL'],
                     a: {
-                      display: 'inline-block',
-                    },
+                      display: 'inline-block'
+                    }
                   }}
                 >
-                  <LinkCallout to={field_make_an_appointment.uri} icon="today">
+                  <LinkCallout to={fieldMakeAnAppointment.uri} icon='today'>
                     Make an appointment with us
                   </LinkCallout>
                 </div>
               )}
 
               {body && (
-                <React.Fragment>
-                  <Heading level={2} size="M">
+                <>
+                  <Heading level={2} size='M'>
                     What we do
                   </Heading>
                   <Html html={body.processed} />
-                </React.Fragment>
+                </>
               )}
 
-              {field_what_were_working_on && (
-                <React.Fragment>
-                  <Heading level={2} size="M">
+              {fieldWhatWereWorkingOn && (
+                <>
+                  <Heading level={2} size='M'>
                     What we're working on
                   </Heading>
-                  <Html html={field_what_were_working_on.processed} />
-                </React.Fragment>
+                  <Html html={fieldWhatWereWorkingOn.processed} />
+                </>
               )}
             </Prose>
 
@@ -134,39 +137,62 @@ function DepartmentTemplate({ data }) {
   );
 }
 
+DepartmentTemplate.propTypes = {
+  data: PropTypes.object
+};
+
 export default DepartmentTemplate;
 
-function processDepartmentInfo({ node, staffDirectorySlug }) {
-  const { relationships, field_email, field_fax_number, title } = node;
+function processDepartmentInfo ({ node, staffDirectorySlug }) {
+  const { relationships, field_email: fieldEmail, field_fax_number: fieldFaxNumber, title } = node;
 
+  console.log(relationships);
+
+  // FOR TESTING! DELETE ME!
+  relationships.field_department_head = [
+    { name: 'marieka', field_user_display_name: 'Marieka Kaye' },
+    { name: 'lstuch', field_user_display_name: 'Lance Thomas Stuchell' },
+    { name: 'lstuch', field_user_display_name: 'Lance Thomas Stuchell' },
+    { name: 'lstuch', field_user_display_name: 'Lance Thomas Stuchell' },
+    { name: 'josalaza', field_user_display_name: 'Josh Salazar' }
+
+  ];
   const leadership = relationships.field_department_head
     ? {
         icon: 'person_outline',
         content: (
-          <React.Fragment>
+          <>
             Leadership:{' '}
-            <Link to={'/users/' + relationships.field_department_head.name}>
-              {relationships.field_department_head.field_user_display_name}
-            </Link>
-          </React.Fragment>
-        ),
+            {relationships.field_department_head.map((departmentHead, index, arr) => {
+              return (
+                <React.Fragment key={index}>
+                  {index + 1 === arr.length && arr.length > 1 ? ' and ' : null}
+                  <Link to={'/users/' + departmentHead.name}>
+                    {departmentHead.field_user_display_name}
+                  </Link>
+                  {index + 1 === arr.length ? null : arr.length > 2 ? ', ' : null}
+                </React.Fragment>
+              );
+            })}
+          </>
+        )
       }
     : null;
-  const email = field_email
+  const email = fieldEmail
     ? {
         icon: 'mail_outline',
         content: (
-          <React.Fragment>
-            Email: <Link to={'mailto:' + field_email}>{field_email}</Link>
-          </React.Fragment>
-        ),
+          <>
+            Email: <Link to={'mailto:' + fieldEmail}>{fieldEmail}</Link>
+          </>
+        )
       }
     : null;
 
-  const fax = field_fax_number
+  const fax = fieldFaxNumber
     ? {
         d: 'M 18 3 V 7 H 6 V 3 Z M 21.6 9.52 a 3.16 3.16 0 0 0 -1.11 -1.1 A 2.93 2.93 0 0 0 19 8 H 5 a 2.91 2.91 0 0 0 -1.49 0.41 a 3.08 3.08 0 0 0 -1.11 1.1 A 3 3 0 0 0 2 11 v 6 H 6 v 4 H 18 V 17 h 4 V 11 A 3 3 0 0 0 21.6 9.52 Z M 16 19 H 8 V 14 h 8 Z m 3.74 -7.28 A 1 1 0 0 1 19 12 a 1 1 0 0 1 -0.7 -0.28 A 0.94 0.94 0 0 1 18 11 a 1.07 1.07 0 0 1 0.28 -0.73 A 0.91 0.91 0 0 1 19 10 a 1 1 0 0 1 0.73 0.31 A 1 1 0 0 1 20 11 A 0.92 0.92 0 0 1 19.73 11.71 Z',
-        content: <React.Fragment>Fax: {field_fax_number}</React.Fragment>,
+        content: <>Fax: {fieldFaxNumber}</>
       }
     : null;
 
@@ -182,34 +208,38 @@ function processDepartmentInfo({ node, staffDirectorySlug }) {
           >
             Organization chart
           </DSLink>
-        ),
+        )
       }
     : null;
 
   const directory = {
     icon: 'people_outline',
     content: (
-      <React.Fragment>
+      <>
         <Link
           to={staffDirectorySlug + '?' + stringifyState({ department: title })}
         >
           Staff directory
         </Link>{' '}
         for {title}
-      </React.Fragment>
-    ),
+      </>
+    )
   };
 
   const deptartmentInfo = [leadership, email, fax, org, directory].filter(
-    (info) => info !== null
+    (info) => {
+      return info !== null;
+    }
   );
 
   return deptartmentInfo;
 }
 
-export function Head({ data }) {
+/* eslint-disable react/prop-types */
+export function Head ({ data }) {
   return <SearchEngineOptimization data={data.department} />;
 }
+/* eslint-enable react/prop-types */
 
 export const query = graphql`
   query ($slug: String!) {
