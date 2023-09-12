e8c17921b184fd405e081236c96544a14ec9e5bc
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2023-09-12 17:47:59 -0400

    September 2023 dependency updates.

--- src/components/html.js
+++ src/components/html.js
@@ -13,111 +13,149 @@ import Table from './table';
 /**
   Headings
 */
-const Heading2 = ({ children, ...other }) => (
-  <Heading level={2} size="M" {...other}>
-    {children}
-  </Heading>
-);
-const Heading3 = ({ children, ...other }) => (
-  <Heading level={3} size="S" {...other}>
-    {children}
-  </Heading>
-);
-const Heading4 = ({ children, ...other }) => (
-  <Heading
-    level={4}
-    size="3XS"
-    style={{ color: COLORS.neutral['300'] }}
-    {...other}
-  >
-    {children}
-  </Heading>
-);
-const Heading5 = ({ children, ...other }) => (
-  <Heading level={5} size="3XS" {...other}>
-    {children}
-  </Heading>
-);
-const Heading6 = ({ children, ...other }) => (
-  <Heading level={6} size="3XS" {...other}>
-    {children}
-  </Heading>
-);
+const Heading2 = ({ children, ...other }) => {
+  return (
+    <Heading level={2} size='M' {...other}>
+      {children}
+    </Heading>
+  );
+};
+const Heading3 = ({ children, ...other }) => {
+  return (
+    <Heading level={3} size='S' {...other}>
+      {children}
+    </Heading>
+  );
+};
+const Heading4 = ({ children, ...other }) => {
+  return (
+    <Heading
+      level={4}
+      size='3XS'
+      style={{ color: COLORS.neutral['300'] }}
+      {...other}
+    >
+      {children}
+    </Heading>
+  );
+};
+const Heading5 = ({ children, ...other }) => {
+  return (
+    <Heading level={5} size='3XS' {...other}>
+      {children}
+    </Heading>
+  );
+};
+const Heading6 = ({ children, ...other }) => {
+  return (
+    <Heading level={6} size='3XS' {...other}>
+      {children}
+    </Heading>
+  );
+};
 
-const renderHast = new rehypeReact({
-  components: {
-    h2: Heading2,
-    h3: Heading3,
-    h4: Heading4,
-    h5: Heading5,
-    h6: Heading6,
-    a: ({ children, href, ...other }) => {
-      if (!children || !href) {
+const renderHast = () => {
+  return new rehypeReact({
+    components: {
+      h2: Heading2,
+      h3: Heading3,
+      h4: Heading4,
+      h5: Heading5,
+      h6: Heading6,
+      a: ({ children, href, ...other }) => {
+        if (!children || !href) {
         // Don't render links without a label or href
-        return null;
-      }
-      return <Link to={href}>{children}</Link>;
-    },
-    p: ({ children, className }) => {
-      if (className === 'umich-lib-callout') {
-        return <Callout>{children}</Callout>;
-      }
+          return null;
+        }
+        return <Link to={href}>{children}</Link>;
+      },
+      p: ({ children, className }) => {
+        if (className === 'umich-lib-callout') {
+          return <Callout>{children}</Callout>;
+        }
 
-      if (className === 'umich-lib-alert') {
+        if (className === 'umich-lib-alert') {
+          return (
+            <Callout intent='warning' alert>
+              {children}
+            </Callout>
+          );
+        }
+
+        return <Text>{children}</Text>;
+      },
+      strong: ({ children }) => {
         return (
-          <Callout intent="warning" alert={true}>
-            {children}
-          </Callout>
+          <strong css={{ fontWeight: '800' }}>{children}</strong>
         );
+      },
+      ul: ({ children }) => {
+        return <List type='bulleted'>{children}</List>;
+      },
+      ol: ({ children }) => {
+        return <List type='numbered'>{children}</List>;
+      },
+      br: () => {
+        return <br />;
+      },
+      em: (props) => {
+        return <em {...props} css={{ fontStyle: 'italic' }} />;
+      },
+      text: Text,
+      lede: ({ children, ...other }) => {
+        return (
+          <Text lede {...other}>
+            {children}
+          </Text>
+        );
+      },
+      u: ({ children }) => {
+        return children;
+      },
+      blockquote: (props) => {
+        return <Blockquote {...props} />;
+      },
+      'drupal-entity': (props) => {
+        return <DrupalEntity {...props} />;
+      },
+      iframe: () => {
+        return null;
+      },
+      article: () => {
+        return null;
+      },
+      figure: (props) => {
+        return <figure {...props} css={{ maxWidth: '38rem' }} />;
+      },
+      figcaption: (props) => {
+        return (
+          <figcaption
+            {...props}
+            css={{
+              color: COLORS.neutral['300']
+            }}
+          />
+        );
+      },
+      table: Table
+    },
+
+    // A workaround to replace the container div created by rehype-react with a React fragment.
+    createElement: (component, props = {}, children = []) => {
+      if (props['data-entity-uuid']) {
+        return <DrupalEntity {...props} />;
       }
 
-      return <Text>{children}</Text>;
-    },
-    strong: ({ children }) => (
-      <strong css={{ fontWeight: '800' }}>{children}</strong>
-    ),
-    ul: ({ children }) => <List type="bulleted">{children}</List>,
-    ol: ({ children }) => <List type="numbered">{children}</List>,
-    br: () => <br />,
-    em: (props) => <em {...props} css={{ fontStyle: 'italic' }} />,
-    text: Text,
-    lede: ({ children, ...other }) => (
-      <Text lede {...other}>
-        {children}
-      </Text>
-    ),
-    u: ({ children }) => children,
-    blockquote: (props) => <Blockquote {...props} />,
-    'drupal-entity': (props) => <DrupalEntity {...props} />,
-    iframe: () => null,
-    article: () => null,
-    figure: (props) => <figure {...props} css={{ maxWidth: '38rem' }} />,
-    figcaption: (props) => (
-      <figcaption
-        {...props}
-        css={{
-          color: COLORS.neutral['300'],
-        }}
-      />
-    ),
-    table: Table,
-  },
+      if (component === 'div') {
+        return <React.Fragment {...props}>{children}</React.Fragment>;
+      }
 
-  // A workaround to replace the container div created by rehype-react with a React fragment.
-  createElement: (component, props = {}, children = []) => {
-    if (props['data-entity-uuid']) {
-      return <DrupalEntity {...props} />;
+      return React.createElement(component, props, children);
     }
+  }).Compiler;
+};
 
-    if (component === 'div') {
-      return <React.Fragment {...props}>{children}</React.Fragment>;
-    }
-
-    return React.createElement(component, props, children);
-  },
-}).Compiler;
-
-function Html({ html, ...rest }) {
+function Html ({ html, ...rest }) {
   const tree = unified().use(rehype, { fragment: true }).parse(html);
 
   return <Prose {...rest}>{renderHast(tree)}</Prose>;
