86b0d42506141746239949ee9b9fb2c322ebdb28
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-02-07 11:49:14 -0500

    Cache Testing2

--- src/components/footer.js
+++ src/components/footer.js
@@ -29,7 +29,7 @@ const links = [
         d: icons.address
       },
       {
-        text: '(734) 764-04012',
+        text: '(734) 764-0401',
         to: 'tel:+1-734-764-0401',
         d: icons.phone
       },
