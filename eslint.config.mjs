20e1aab62683d35e7713ffe9af0de46591a97b6c
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-05-17 11:28:44 -0400

    updated packages
    additional eslint updates and another gatsby version dropped

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+//import { fixupConfigRules } from '@eslint/compat';
+import stylistic from '@stylistic/eslint-plugin';
+import pluginJs from "@eslint/js";
+import react from 'eslint-plugin-react';
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+       parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        globals: globals.es2021
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      react,
+    },
+    rules: {
+      ...pluginJs.configs.all.rules,
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+    }
+  },
+  stylistic.configs['recommended-flat'],
+  pluginJs.configs.recommended,
+];
