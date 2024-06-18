2f5eb38112cd0842e478c81133856be202d92deb
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-06-18 14:56:00 -0400

    Update use-state.js
    1174 -> 1169

--- src/components/use-state.js
+++ src/components/use-state.js
@@ -1,14 +1,21 @@
 import React, { createContext, useContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
 
 const StateContext = createContext();
 
-export function StateProvider ({ reducer, initialState, children }) {
+export const StateProvider = ({ reducer, initialState, children }) => {
   return (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
       {children}
     </StateContext.Provider>
   );
-}
+};
+
+StateProvider.propTypes = {
+  children: PropTypes.node,
+  initialState: PropTypes.object,
+  reducer: PropTypes.object
+};
 
 export const useStateValue = () => {
   return useContext(StateContext);
