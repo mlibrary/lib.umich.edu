ff0337d1914ba5d9cfe0411b6f9e397ce9a98342
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-07-09 09:41:17 -0400

    input.js
    996 -> 980

--- src/reusable/text-input.js
+++ src/reusable/text-input.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
 import { COLORS, Icon, Input } from '../reusable';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from '@emotion/styled';
 
 const StyledFormItem = styled('div')(
   {
-    width: '100%',
-    boxSizing: 'border-box'
+    boxSizing: 'border-box',
+    width: '100%'
   },
   ({ invalid }) => {
     return {
@@ -17,37 +17,36 @@ const StyledFormItem = styled('div')(
 );
 
 const StyledLabel = styled('label')({
+  boxSizing: 'border-box',
   display: 'block',
-  marginBottom: '0.5rem',
-  boxSizing: 'border-box'
+  marginBottom: '0.5rem'
 });
 
 const StyledFormItemDescription = styled('span')({
-  display: 'block',
-  color: COLORS.neutral[400]
+  color: COLORS.neutral[400],
+  display: 'block'
 });
 
 const StyledFormItemErrorMessage = styled('p')({
-  marginTop: '0.5rem',
   color: COLORS.orange[400],
-  fontWeight: '600'
+  fontWeight: '600',
+  marginTop: '0.5rem'
 });
 
 /**
  *Use this when you need to let users enter text that's no longer than a single line.
  */
 const TextInput = ({
-  labelText,
   descriptionText,
-  className,
-  id,
-  placeholder,
-  type,
-  onChange,
-  onClick,
   hideLabel,
+  id,
   invalid,
   invalidText,
+  labelText,
+  onChange,
+  onClick,
+  placeholder,
+  type,
   ...other
 }) => {
   const textInputProps = {
@@ -74,7 +73,6 @@ const TextInput = ({
       )
     : null;
 
-  // TODO: add hidden style
   const label = labelText
     ? (
       <StyledLabel htmlFor={id}>
@@ -123,28 +121,31 @@ const TextInput = ({
 };
 
 TextInput.propTypes = {
-  id: PropTypes.string.isRequired,
-  labelText: PropTypes.node.isRequired,
-  hideLabel: PropTypes.bool,
   className: PropTypes.string,
   defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  descriptionText: PropTypes.string,
   disabled: PropTypes.bool,
+  hideLabel: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  invalid: PropTypes.bool,
+  invalidText: PropTypes.string,
+  labelText: PropTypes.node.isRequired,
   onChange: PropTypes.func,
   onClick: PropTypes.func,
   placeholder: PropTypes.string,
   type: PropTypes.string,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  invalid: PropTypes.bool,
-  invalidText: PropTypes.string
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
+/* eslint-disable no-empty-function */
 TextInput.defaultProps = {
   disabled: false,
-  type: 'text',
+  invalid: false,
+  invalidText: '',
   onChange: () => {},
   onClick: () => {},
-  invalid: false,
-  invalidText: ''
+  type: 'text'
 };
+/* eslint-enable no-empty-function */
 
 export default TextInput;
