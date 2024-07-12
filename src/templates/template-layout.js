8e7e4768093aaf22db214dac8417bc848170bae7
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-07-12 13:40:56 -0400

    Update template-layout.js
    284 -> 280

--- src/templates/template-layout.js
+++ src/templates/template-layout.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import Layout from '../components/layout';
+import PropTypes from 'prop-types';
+import React from 'react';
 import SearchEngineOptimization from '../components/seo';
 
 export default function TemplateLayout ({ node, children, ...rest }) {
@@ -9,6 +10,14 @@ export default function TemplateLayout ({ node, children, ...rest }) {
     </Layout>
   );
 }
+
+TemplateLayout.propTypes = {
+  children: PropTypes.any,
+  node: PropTypes.shape({
+    // eslint-disable-next-line camelcase
+    drupal_internal__nid: PropTypes.any
+  })
+};
 
 /* eslint-disable react/prop-types */
 export const Head = ({ node }) => {
