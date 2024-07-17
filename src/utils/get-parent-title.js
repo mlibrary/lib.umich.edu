744ccd412e4a0d0ffcab45e0f0559222c8135a47
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-07-17 11:57:29 -0400

    Update get-parent-title.js
    116 -> 109

--- src/utils/get-parent-title.js
+++ src/utils/get-parent-title.js
@@ -1,9 +1,11 @@
 export default function getParentTitle ({ node }) {
-  const { field_parent_location, field_room_building } = node.relationships;
+  const { field_parent_location: fieldParentLocation, field_room_building: fieldRoomBuilding } = node.relationships;
 
-  return field_parent_location
-    ? field_parent_location.title
-    : field_room_building
-      ? field_room_building.title
-      : null;
+  if (fieldParentLocation) {
+    return fieldParentLocation.title;
+  } else if (fieldRoomBuilding) {
+    return fieldRoomBuilding.title;
+  }
+
+  return null;
 }
