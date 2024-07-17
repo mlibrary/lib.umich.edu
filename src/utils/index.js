6706cf138ade81e098127a19fddd2740119b4864
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-07-17 12:09:58 -0400

    Update index.js
    76 - 75

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
-import getImage from './get-image';
-import getParentTitle from './get-parent-title';
 import getFloor from './get-floor';
+import getImage from './get-image';
 import getNode from './get-node';
+import getParentTitle from './get-parent-title';
 import getRoom from './get-room';
 
 export { getNode, getImage, getFloor, getParentTitle, getRoom };
