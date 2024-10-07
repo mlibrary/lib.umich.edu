340adfdad96020ed314732203bba6a94e775f4d8
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-10-07 12:28:48 -0400

    Update gatsby-node.js

--- plugins/gatsby-source-umich-lib/gatsby-node.js
+++ plugins/gatsby-source-umich-lib/gatsby-node.js
@@ -479,12 +479,11 @@ exports.createPages = ({ actions, graphql }) => {
               }
             }
            profiles: allUserUser(
-              filter: { field_make_profile_public: { eq: true } }
+              filter: {name: {ne: null}}
             ) {
               edges {
                 node {
                   __typename
-                  field_make_profile_public
                   field_user_display_name
                   field_user_work_title
                   name
