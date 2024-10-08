05cc0a8d87c95e12fdd7c97ef1ae853e9cd0be90
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-10-08 13:10:53 -0400

    using Gatsby Script component

---
+++ gatsby-shared.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import SkipLinks from './src/components/skip-links';
+import { Script } from 'gatsby';
+
+export const wrapPageElement = ({ element }) => {
+  return (
+    <>
+      <Script async type='text/javascript' src='https://umich.edu/apis/umalerts/umalerts.js' />
+      <Script
+        type='module'
+        src='https://cdn.jsdelivr.net/npm/@umich-lib/web@latest/dist/umich-lib/umich-lib.esm.js'
+        strategy='idle'
+        onLoad={() => {
+          return console.log('success');
+        }}
+        onError={() => {
+          return console.log('sadness');
+        }}
+      />
+      <div
+        css={{
+          minHeight: '100%',
+          display: 'grid',
+          gridTemplateRows: 'auto auto 1fr',
+          gridTemplateColumns: '100%'
+        }}
+      >
+        <div>
+          <SkipLinks />
+          <m-universal-header></m-universal-header>
+        </div>
+        {element}
+      </div>
+      <m-chat id='chat'></m-chat>
+    </>
+  );
+};
