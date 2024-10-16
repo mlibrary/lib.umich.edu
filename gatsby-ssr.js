1dd76e06a43e2ed6cc5fce562f716c64a3852b24
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-10-16 10:33:47 -0400

    forgot the comma

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,12 +3,12 @@ export { wrapPageElement } from './gatsby-shared';
 export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   setHeadComponents([
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css' />
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css' />,
     <link
-      key="umich-lib-preload"
-      rel="preload"
-      href="https://cdn.jsdelivr.net/npm/@umich-lib/web@latest/dist/umich-lib/umich-lib.esm.js"
-      as="script"
+      key='umich-lib-preload'
+      rel='preload'
+      href='https://cdn.jsdelivr.net/npm/@umich-lib/web@latest/dist/umich-lib/umich-lib.esm.js'
+      as='script'
     />
   ]);
 };
