e3d22ddbd5312c5d2edbda1f13a5783dae319433
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-10-16 10:30:01 -0400

    preloading webcomponents test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,5 +4,11 @@ export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   setHeadComponents([
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css' />
+    <link
+      key="umich-lib-preload"
+      rel="preload"
+      href="https://cdn.jsdelivr.net/npm/@umich-lib/web@latest/dist/umich-lib/umich-lib.esm.js"
+      as="script"
+    />
   ]);
 };
