0bdd1c23336ad218a0c1edd50e14b7012ab84ded
Joshua Salazar <6354268+SalazarJosh@users.noreply.github.com>
2024-12-19 14:49:03 -0500

    Update call-to-action.js

--- src/reusable/call-to-action.js
+++ src/reusable/call-to-action.js
@@ -11,7 +11,7 @@ export default function CallToAction ({ children, ...rest }) {
         borderRadius: '4px',
         fontWeight: 'bold',
         maxWidth: '38rem',
-        padding: '1rem, 1.5rem, 1rem, 1rem',
+        padding: `${SPACING.M} ${SPACING.L} ${SPACING.M} ${SPACING.M}`,
         width: 'fit-content'
       }}
       data-umich-cta
