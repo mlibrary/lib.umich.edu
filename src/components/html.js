5dcce69f0e93aaebdce5faf8d96c34c65fcde13b
Josh Salazar <6354268+SalazarJosh@users.noreply.github.com>
2025-04-07 16:12:48 -0400

    testing html.js update

--- src/components/html.js
+++ src/components/html.js
@@ -1,5 +1,7 @@
 /* eslint-disable id-length */
+import * as prod from 'react/jsx-runtime';
 import { Heading, List, Text } from '../reusable';
+import React, { createElement, Fragment, useEffect, useState } from 'react';
 import Blockquote from '../reusable/blockquote';
 import Callout from '../reusable/callout';
 import CallToAction from '../reusable/call-to-action';
@@ -7,8 +9,7 @@ import DrupalEntity from './drupal-entity';
 import Link from './link';
 import PropTypes from 'prop-types';
 import Prose from './prose';
-import React from 'react';
-import rehype from 'rehype-parse';
+import rehypeParse from 'rehype-parse';
 import rehypeReact from 'rehype-react';
 import Table from './table';
 import { unified } from 'unified';
@@ -78,117 +79,131 @@ Heading6.propTypes = {
   children: PropTypes.any
 };
 
-// eslint-disable-next-line new-cap
-const renderHast = new rehypeReact({
-  components: {
-    a: ({ children, href }) => {
-      if (!children || !href) {
-        // Don't render links without a label or href
-        return null;
-      }
-      return <Link to={href}>{children}</Link>;
-    },
-    article: () => {
+const components = {
+  a: ({ children, href }) => {
+    if (!children || !href) {
       return null;
-    },
-    blockquote: (props) => {
-      return <Blockquote {...props} />;
-    },
-    br: () => {
-      return <br />;
-    },
-    'drupal-entity': (props) => {
-      return <DrupalEntity {...props} />;
-    },
-    em: (props) => {
-      return <em {...props} css={{ fontStyle: 'italic' }} />;
-    },
-    figcaption: (props) => {
-      return (
-        <figcaption
-          {...props}
-          css={{
-            color: 'var(--color-neutral-300)'
-          }}
-        />
-      );
-    },
-    figure: (props) => {
-      return <figure {...props} css={{ maxWidth: '38rem' }} />;
-    },
-    h2: Heading2,
-    h3: Heading3,
-    h4: Heading4,
-    h5: Heading5,
-    h6: Heading6,
-    iframe: () => {
-      return null;
-    },
-    lede: ({ children, ...other }) => {
-      return (
-        <Text lede {...other}>
-          {children}
-        </Text>
-      );
-    },
-    ol: ({ children }) => {
-      return <List type='numbered'>{children}</List>;
-    },
-    p: ({ children, className }) => {
-      if (className === 'umich-lib-callout') {
-        return <Callout>{children}</Callout>;
-      }
-
-      if (className === 'umich-lib-alert') {
-        return (
-          <Callout intent='warning' alert={true}>
-            {children}
-          </Callout>
-        );
-      }
-      if (className === 'umich-lib-cta') {
-        return <CallToAction>{children}</CallToAction>;
-      }
-
-      return <Text>{children}</Text>;
-    },
-    strong: ({ children }) => {
-      return (
-        <strong css={{ fontWeight: '800' }}>{children}</strong>
-      );
-    },
-    table: Table,
-    text: Text,
-    u: ({ children }) => {
-      return children;
-    },
-    ul: ({ children }) => {
-      return <List type='bulleted'>{children}</List>;
     }
+    return <Link to={href}>{children}</Link>;
   },
-
-  // A workaround to replace the container div created by rehype-react with a React fragment.
-  createElement: (component, props = {}, children = []) => {
-    if (props['data-entity-uuid']) {
-      return <DrupalEntity {...props} />;
+  article: () => {
+    return null;
+  },
+  blockquote: (props) => {
+    return <Blockquote {...props} />;
+  },
+  br: () => {
+    return <br />;
+  },
+  'drupal-entity': (props) => {
+    return <DrupalEntity {...props} />;
+  },
+  em: (props) => {
+    return <em {...props} css={{ fontStyle: 'italic' }} />;
+  },
+  figcaption: (props) => {
+    return (
+      <figcaption
+        {...props}
+        css={{
+          color: 'var(--color-neutral-300)'
+        }}
+      />
+    );
+  },
+  figure: (props) => {
+    return <figure {...props} css={{ maxWidth: '38rem' }} />;
+  },
+  h2: Heading2,
+  h3: Heading3,
+  h4: Heading4,
+  h5: Heading5,
+  h6: Heading6,
+  iframe: () => {
+    return null;
+  },
+  img: (props) => {
+    return <img {...props} />;
+  },
+  lede: ({ children, ...other }) => {
+    return (
+      <Text lede {...other}>
+        {children}
+      </Text>
+    );
+  },
+  ol: ({ children }) => {
+    return <List type='numbered'>{children}</List>;
+  },
+  p: ({ children, className }) => {
+    if (className === 'umich-lib-callout') {
+      return <Callout>{children}</Callout>;
     }
-
-    if (component === 'div') {
-      return <React.Fragment {...props}>{children}</React.Fragment>;
+    if (className === 'umich-lib-alert') {
+      return (
+        <Callout intent='warning' alert={true}>
+          {children}
+        </Callout>
+      );
     }
-
-    return React.createElement(component, props, children);
+    if (className === 'umich-lib-cta') {
+      return <CallToAction>{children}</CallToAction>;
+    }
+    return <Text>{children}</Text>;
+  },
+  strong: ({ children }) => {
+    return (
+      <strong css={{ fontWeight: '800' }}>{children}</strong>
+    );
+  },
+  table: Table,
+  text: Text,
+  u: ({ children }) => {
+    return children;
+  },
+  ul: ({ children }) => {
+    return <List type='bulleted'>{children}</List>;
   }
-}).Compiler;
-
-const Html = ({ html, ...rest }) => {
-  const tree = unified().use(rehype, { fragment: true }).parse(html);
-
-  return <Prose {...rest}>{renderHast(tree)}</Prose>;
 };
+
+// Define your custom render logic
+const processor = (text) => {
+  return unified()
+    .use(rehypeParse, { fragment: true })
+    .use(rehypeReact, {
+      Fragment: prod.Fragment,
+      components,
+      createElement: (component, props = {}, children = []) => {
+        if (props['data-entity-uuid']) {
+          return <DrupalEntity {...props} />;
+        }
+
+        // Optional: unwrap <div>s that are inside <p>
+        if (component === 'div') {
+          return <Fragment {...props}>{children}</Fragment>;
+        }
+
+        return createElement(component, props, children);
+      },
+      jsx: prod.jsx,
+      jsxs: prod.jsxs
+    })
+    .process(text);
+};
+
+export default function Html ({ html, ...rest }) {
+  const [content, setContent] = useState(<></>);
+
+  useEffect(() => {
+    ;(async () => {
+      const file = await processor(html);
+      setContent(file.result);
+    })();
+  }, [html]);
+  console.log(content);
+  return <Prose {...rest}>{content}</Prose>;
+}
 
 Html.propTypes = {
   html: PropTypes.any
 };
-
-export default Html;
