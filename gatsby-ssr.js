d6b2176390efa715cb482496689e2fae2d198951
Josh Salazar <6354268+SalazarJosh@users.noreply.github.com>
2025-04-24 11:14:24 -0400

    added DS components

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,11 +5,11 @@ export { wrapPageElement } from './gatsby-shared';
 export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   setHeadComponents([
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@umich-lib/css@1.0.9/dist/umich-lib.css' key='umich-lib-stylesheet' />,
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@umich-lib/design-system@latest/dist/umich-lib.min.css' key='umich-lib-stylesheet' />,
     <link
       key='umich-lib-preload'
       rel='preload'
-      href='https://cdn.jsdelivr.net/npm/@umich-lib/web@latest/dist/umich-lib/umich-lib.esm.js'
+      href='https://cdn.jsdelivr.net/npm/@umich-lib/design-system@latest/dist/umich-lib-components.min.js'
       as='script'
     />
   ]);
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,99 @@
 import HeaderLargeScreen from './header-largescreen';
 import HeaderSmallScreen from './header-smallscreen';
+import { Icon, LINK_STYLES, SPACING } from '../../reusable';
+import PlainLink from '../plain-link';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const Header = ({ primary, secondary }) => {
   return (
     <React.Fragment>
+      <m-website-header variant='dark'>
+        <nav>
+          <ul>
+            {secondary.map(({ text, to, icon }, iterator) => {
+              return (
+                <li
+                  css={{
+                    ':not(:last-child)': {
+                      marginRight: SPACING.M
+                    },
+                    display: 'inline-block'
+                  }}
+                  key={iterator + text}
+                >
+                  <PlainLink
+                    to={to}
+                    external={to === '/my-account'}
+                    css={{
+                      ...LINK_STYLES.special,
+                      ':hover': {
+                        '.text': LINK_STYLES.special[':hover']
+                      },
+                      padding: `${SPACING.S} 0`
+                    }}
+                  >
+                    {icon && (
+                      <Icon
+                        icon={icon}
+                        size={14}
+                        css={{
+                          marginRight: SPACING['2XS'],
+                          marginTop: '-2px'
+                        }}
+                      />
+                    )}
+                    <span className='text'>{text}</span>
+                  </PlainLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </m-website-header>
+      <m-website-header>
+        <nav>
+          <ul>
+            {secondary.map(({ text, to, icon }, iterator) => {
+              return (
+                <li
+                  css={{
+                    ':not(:last-child)': {
+                      marginRight: SPACING.M
+                    },
+                    display: 'inline-block'
+                  }}
+                  key={iterator + text}
+                >
+                  <PlainLink
+                    to={to}
+                    external={to === '/my-account'}
+                    css={{
+                      ...LINK_STYLES.special,
+                      ':hover': {
+                        '.text': LINK_STYLES.special[':hover']
+                      },
+                      padding: `${SPACING.S} 0`
+                    }}
+                  >
+                    {icon && (
+                      <Icon
+                        icon={icon}
+                        size={14}
+                        css={{
+                          marginRight: SPACING['2XS'],
+                          marginTop: '-2px'
+                        }}
+                      />
+                    )}
+                    <span className='text'>{text}</span>
+                  </PlainLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </m-website-header>
       <HeaderSmallScreen primary={primary} secondary={secondary} />
       <HeaderLargeScreen primary={primary} secondary={secondary} />
     </React.Fragment>
