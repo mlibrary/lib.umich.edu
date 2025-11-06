2bb8a53f39d939ea95f583dd061d638c14ac3977
Josh Salazar <6354268+SalazarJosh@users.noreply.github.com>
2025-11-06 15:32:28 -0500

    Remove ReactPlayer and use custom YouTube video player

--- src/components/media-player.js
+++ src/components/media-player.js
@@ -1,11 +1,43 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ReactPlayer from 'react-player';
+
+const getYouTubeVideoId = (url) => {
+  if (!url) {
+    return null;
+  }
+
+  const regex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)(?<videoId>[^"&?/\s]{11})/u;
+  const match = url.match(regex);
+  return match ? match.groups.videoId : null;
+};
 
 export default function MediaPlayer ({ url }) {
   if (!url) {
     return null;
   }
+
+  const videoId = getYouTubeVideoId(url);
+
+  if (!videoId) {
+    return (
+      <div css={{ backgroundColor: '#f5f5f5', padding: '20px', textAlign: 'center' }}>
+        <p>Unable to load video. Please check the URL format.</p>
+      </div>
+    );
+  }
+
+  const getOrigin = () => {
+    if (typeof window !== 'undefined') {
+      return window.location.origin;
+    }
+    return 'https://lib.umich.edu';
+  };
+
+  const embedUrl = `https://www.youtube.com/embed/${videoId}?`
+    + `origin=${encodeURIComponent(getOrigin())}&`
+    + `enablejsapi=1&`
+    + `modestbranding=1&`
+    + `rel=0`;
 
   return (
     <div
@@ -14,11 +46,14 @@ export default function MediaPlayer ({ url }) {
         position: 'relative'
       }}
     >
-      <ReactPlayer
-        src={url}
+      <iframe
+        src={embedUrl}
+        title='YouTube video player'
         width='100%'
         height='100%'
-        controls={true}
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen
         css={{
           left: '0',
           position: 'absolute',
