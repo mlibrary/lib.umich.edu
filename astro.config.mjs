2470e3564f9c2edacab756c2ec88d82e108d9759
Josh Salazar <6354268+SalazarJosh@users.noreply.github.com>
2026-01-22 10:49:42 -0500

    Search results UI cleanup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // No integrations needed
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
