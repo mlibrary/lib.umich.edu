752baf68b95d0e1918407b9ece70200698616946
Josh Salazar <6354268+SalazarJosh@users.noreply.github.com>
2026-04-29 11:27:49 -0400

    usernames added to lunr index

--- plugins/search-index-integration.js
+++ plugins/search-index-integration.js
@@ -4,6 +4,7 @@
  */
 import { mkdirSync, writeFileSync } from 'node:fs';
 import { getPagesToGenerate } from '../src/lib/page-generator.js';
+import { getAllUsersForProfiles } from '../src/lib/user-data.js';
 import { join } from 'node:path';
 import lunr from 'lunr';
 
@@ -13,7 +14,10 @@ export default function searchIndexIntegration() {
       'astro:build:done': async ({ dir }) => {
         console.log('[search-index] Generating search index…');
 
-        const pages = await getPagesToGenerate();
+        const [pages, { users }] = await Promise.all([
+          getPagesToGenerate(),
+          getAllUsersForProfiles()
+        ]);
 
         const store = {};
         const docs = [];
@@ -26,7 +30,8 @@ export default function searchIndexIntegration() {
               keywords: page.keywords || '',
               summary: page.summary || '',
               tag: page.tag || '',
-              title: page.title || ''
+              title: page.title || '',
+              uniqname: ''
             };
 
             docs.push(doc);
@@ -39,12 +44,35 @@ export default function searchIndexIntegration() {
           }
         }
 
+        for (const user of (users || [])) {
+          const uniqname = user.attributes?.name;
+          if (!uniqname) continue;
+          const ref = `SitePage /users/${uniqname}`;
+          const doc = {
+            id: ref,
+            keywords: '',
+            summary: user.attributes?.field_user_work_title || '',
+            tag: '',
+            title: user.attributes?.field_user_display_name || uniqname,
+            uniqname
+          };
+
+          docs.push(doc);
+          store[ref] = {
+            keywords: doc.keywords,
+            summary: doc.summary,
+            tag: doc.tag,
+            title: doc.title
+          };
+        }
+
         const idx = lunr(function () {
           this.ref('id');
           this.field('title', { boost: 9 });
           this.field('summary', { boost: 3 });
           this.field('keywords');
           this.field('tag');
+          this.field('uniqname');
 
           docs.forEach((doc) => {
             this.add(doc);
